04c90a3a58a412199efb9a4fd61302539e11df89
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-05-11 14:25:19 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const hamburgerItem = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+const closeNavMenu = document.querySelector('.fa-times');
+
+hamburgerItem.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburgerEl.classList.toggle('active');
+});
+
+closeNavMenu.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    hamburgerEl.classList.remove('active');
+});
+
+const closeMenu = () => {
+  navMenu.classList.remove('active');
+  hamburgerEl.classList.remove('active');
+}
